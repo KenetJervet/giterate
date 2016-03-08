e8b6fd1cb3007fcf79167c053a15ab5135439b57
Savor d'Isavano <anohigisavay@gmail.com>
2016-03-09 01:18:43 +0800

    Unit test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import           Control.Monad
+import           Giterate.Internal
+import           Test.HUnit
+
+hutest1 :: Test
+hutest1 = TestCase $ do
+  (exitCode, out, err) <- runGitCmd ["status"] ""
+  putStrLn "==== exitCode ===="
+  print exitCode
+  putStrLn "==== out ===="
+  putStrLn out
+  putStrLn "==== err ===="
+  putStrLn err
+  putStrLn "==== FIN ===="
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+-- main = test1
+-- main = quickCheck ((\s -> s == s) :: Char -> Bool)
+main = void $ runTestTT $ TestList [TestLabel "hutest1" hutest1]
