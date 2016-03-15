596e89d20a1966a0c8473229eded72dddbfbc368
Savor d'Isavano <anohigisavay@gmail.com>
2016-03-15 19:33:10 +0800

    Added prerequisite

--- src/Giterate.hs
+++ src/Giterate.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE GADTs           #-}
 {-# LANGUAGE RecordWildCards #-}
 
 module Giterate ( CmdResult
@@ -9,6 +10,8 @@ module Giterate ( CmdResult
 
 import           Data.Time.Clock
 import           Giterate.Internal as GI
+import           System.Exit
+import           System.IO
 
 {- ================= -}
 {- Giterate commands -}
@@ -17,6 +20,29 @@ import           Giterate.Internal as GI
 class GtrCommand cmd where
   execute :: cmd -> IO CmdResult
 
+data GtrCommandCombo a b where
+  GtrCommandCombo :: (GtrCommand a, GtrCommand b) => a -> b -> GtrCommandCombo a b
+
+instance GtrCommand (GtrCommandCombo a b) where
+  execute (GtrCommandCombo a b) = execute a %>> execute b
+
+infixl 1 %%>>
+(%%>>) :: (GtrCommand a, GtrCommand b) => a -> b -> GtrCommandCombo a b
+a %%>> b = GtrCommandCombo a b
+
+----------------------
+-- Repo status checks
+----------------------
+
+data GtrPrereq a where
+  GtrPrereq :: (GtrCommand a) => a -> String -> GtrPrereq a
+
+instance GtrCommand (GtrPrereq a) where
+  execute (GtrPrereq a errmsg) = do
+    cmdResult@(exitCode, out, err) <- execute a
+    case exitCode of
+      ExitSuccess -> undefined
+      _ -> hPutStr stderr errmsg >> return cmdResult
 
 ----------------
 -- Initial repo
